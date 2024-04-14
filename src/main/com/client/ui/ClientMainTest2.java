805d8746a8069bbe9546faaba76af00bfe158b5c
AeLinLee <dldofls666@gmail.com>
2024-04-14 15:29:29 +0900

    client

--- src/main/com/client/ui/ClientMainTest2.java
+++ src/main/com/client/ui/ClientMainTest2.java
@@ -9,19 +9,25 @@ public class ClientMainTest2 {
 
 	public static void main(String[] args) {	
 		
-		
-	
 		SqlSession session = MySqlSessionFactory.openSession();
 		
+		//고객기업 등록
 		
-		
-		ClientDTO dto = new ClientDTO(0, "dd", "ss", 12, "dd");
+		ClientDTO dto = new ClientDTO(122, "dd", "ss", 12, "dd");
 		session.insert("main.resource.mapper.ClientMapper.insert", dto);
 		session.commit();
          System.out.println(dto);
          
          
-         
+         //정보 수정          
+        ClientDTO dto2 = new ClientDTO();  
+        dto2.setCategory("안녕");
+        dto2.setId(122);
+        session.update("main.resource.mapper.ClientMapper.update", dto2);
+        session.commit();
+        System.out.println(dto2);
+        
+        
 	}
 
 }
