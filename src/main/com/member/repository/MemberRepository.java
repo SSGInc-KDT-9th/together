cccf21c1e164419410e463205ff48902c6e12c9f
kimdodo97 <eh4536@naver.com>
2024-04-14 16:40:52 +0900

    fix : Member id 타입 수정 (long -> Long)

--- src/main/com/member/repository/MemberRepository.java
+++ src/main/com/member/repository/MemberRepository.java
@@ -3,7 +3,7 @@ package main.com.member.repository;
 import main.com.member.domain.Member;
 
 public interface MemberRepository {
-    long save(Member member);
+    Long save(Member member);
     Member findByMemberId(String memberId);
     int delete(Member member);
     void update(Member member);
