63bc4bf2c3656ccc0fbcfeffc6570714c0b24780
Parkminji <parkminji5787@gmail.com>
2024-04-15 13:44:45 +0900

    Merge remote-tracking branch 'origin/main' into main

--- src/main/com/store/dao/StoreDAO.java
+++ src/main/com/store/dao/StoreDAO.java
@@ -14,17 +14,15 @@ public class StoreDAO {
         return list;
     }
 
-    //고객기업 id를 통한 정보 수정
     public void update(SqlSession session, StoreDTO StoreDTO) {
         session.update("main.resource.mapper.StoreMapper.update", StoreDTO);
     }
     
-    //고객기업 저장
+
     public int insert(SqlSession session, StoreDTO StoreDTO) {
         return session.insert("main.resource.mapper.StoreMapper.insert", StoreDTO);
     }
-    
-    //고객기업 id를 통한 삭제
+
     public int delete(SqlSession session, int id) {
     	return session.delete("main.resource.mapper.StoreMapper.delete", id);
     }
