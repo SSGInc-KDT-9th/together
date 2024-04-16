879efdcf94f79a067bcbfea31a8468fe04260ea0
kimdodo97 <eh4536@naver.com>
2024-04-16 15:38:56 +0900

    feat : 투게더 메인 앱 구현

---
+++ src/main/com/TogatherMain.java
@@ -0,0 +1,72 @@
+package main.com;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import main.com.client.ui.ClientMain;
+import main.com.product.ui.ProductApp;
+import main.com.sales.SalesMain;
+import main.com.supplier.ui.SupplierMain;
+import main.com.swing.ReleaseMain;
+
+import javax.swing.JTabbedPane;
+
+public class TogatherMain extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel productPanel;
+	private JPanel supplierMain;
+	private JPanel storeMain;
+	private JPanel releasePanel;
+	private JPanel salesPanel;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TogatherMain frame = new TogatherMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public TogatherMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 1280, 720);
+		contentPane.add(tabbedPane);
+
+		ProductApp productApp = new ProductApp();
+		productPanel = productApp.getMainPanel();
+		tabbedPane.addTab("제품", null, productPanel, null);
+
+		supplierMain = new SupplierMain();
+		tabbedPane.addTab("납품기업", null, supplierMain, null);
+
+		storeMain = new ClientMain();
+		tabbedPane.addTab("고객기업", null, storeMain, null);
+
+		ReleaseMain releaseTest = new ReleaseMain();
+		releasePanel = releaseTest.getReleasePanel();
+		tabbedPane.addTab("출고관리", null, releasePanel, null);
+
+		SalesMain salesMain = new SalesMain();
+		salesPanel = salesMain.getMainPanel();
+		tabbedPane.addTab("매출 관리", null, salesPanel, null);
+	}
+}
