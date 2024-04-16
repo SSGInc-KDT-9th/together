bfe105ae4caa5acb48e6cc91d41c5d0d6c65b7e5
kimdodo97 <eh4536@naver.com>
2024-04-16 22:38:51 +0900

    fix : Long Try Parsing 수정

--- src/main/com/util/TryParse.java
+++ src/main/com/util/TryParse.java
@@ -1,5 +1,8 @@
 package main.com.util;
 
+import javax.swing.*;
+import java.text.ParseException;
+
 public class TryParse {
     public static Integer tryParseInt(String str, Integer defaultValue) {
         if (str == null) {
@@ -13,13 +16,14 @@ public class TryParse {
     }
 
     public static Long tryParseLong(String str, Long defaultValue) {
-        if (str == null) {
+        if (str == null || str.isEmpty()) {
             return defaultValue;
         }
         try {
             return Long.parseLong(str);
         } catch (NumberFormatException e) {
-            return defaultValue;
+            JOptionPane.showConfirmDialog(null, "숫자만 입력하세요", "ERROR", JOptionPane.WARNING_MESSAGE);
+            throw new IllegalArgumentException("Parsing 안된느 숫자 입력");
         }
     }
 }
